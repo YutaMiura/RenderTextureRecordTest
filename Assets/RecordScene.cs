4a9e8697c859948ef27fdea39b961a95c820a600
YutaMiura <yuuta.miura@nil-one.com>
2019-06-17 23:12:39 +0900

    テストプロジェクト作成

---
+++ Assets/RecordScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class RecordScene : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer _videoPlayer;
+
+    [SerializeField]
+    private Recorder _recorder;
+
+
+    public IEnumerator OnClickRecord()
+    {
+        _videoPlayer.Play();
+        _recorder.Record();
+        yield return new WaitUntil(() => _videoPlayer.isPaused || !_videoPlayer.isPlaying);
+        _recorder.StopRecord();
+    }
+}
---
+++ Assets/Recorder.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+public class Recorder : MonoBehaviour
+{
+    private Camera _camera;
+
+    private bool _isRecording;
+
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();         
+    }
+
+    // Update is called once per frame
+    public void Record()
+    {
+        ClearFiles();
+        _isRecording = true;
+    }
+
+    public void StopRecord()
+    {
+        _isRecording = false;
+    }
+
+    private void Update()
+    {
+        if (_isRecording)
+        {
+            CapturePerFrame();    
+        }        
+    }
+
+    void ClearFiles()
+    {
+        foreach (var file in Directory.GetFiles("/Users/yutamiura/Desktop/test/"))
+        {
+            File.Delete(file);
+        }
+    }
+
+    void CapturePerFrame()
+    {
+        var tex = _camera.targetTexture;
+        var t = new Texture2D(tex.width, tex.height, TextureFormat.RGB24, false);
+        t.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+        t.Apply();
+
+        var b = t.EncodeToJPG();
+
+        File.WriteAllBytes($"/Users/yutamiura/Desktop/test/{i++}.jpg", b);
+        
+        Destroy(t);
+    }
+
+    void CreateVideoFile()
+    {
+        
+    }
+    
+}
